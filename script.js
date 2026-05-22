9e3b94d7d7262df879b14e3acf1537e16d809a44
jahniasfnnn-ux <jahniasfnnn@gmail.com>
2026-05-22 18:27:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function filterGame(game, element) {
     document.querySelectorAll('.filter-btn').forEach(btn => btn.classList.remove('active'));
     element.classList.add('active');
     document.querySelectorAll('.product-card').forEach(card => {
-        card.style.display = (game === 'الكل' || card.getAttribute('data-game') === game) ? 'block' : 'none';
+        if (game === 'الكل' || card.getAttribute('data-game') === game) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
     });
 }
